9eff8f28d9bf9ef11a9360934be8d7e972e9ace9
majidub2 <majid.mohamed9802@gmail.com>
2022-03-08 14:02:30 +0000

    added more routes

---
+++ application/forms.py
@@ -0,0 +1,22 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField
+from wtforms.validators import Length, NumberRange, InputRequired
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
+from application.models import reviews,restaurants
+
+
+
+class Addreviews(FlaskForm): #many
+    title = StringField("review title", validators=[InputRequired())
+    review = StringField("review body", validators=[InputRequired())
+    rating = IntegerField('Rating (1-5)',validators=[InputRequired(),NumberRange(min=1, max=5)])
+    restaurants_ID = IntegerField('Restaurant to review')
+    submit = SubmitField('Submit your review!')
+
+# class Addrestaraunts(FlaskForm):
+#     name = StringField("restaurant name", validators=[InputRequired())
+#     cuisine = StringField("restaurant cuisine", validators=[InputRequired())
+#     submit = SubmitField('Enter Restaurant')
+
+    
+
