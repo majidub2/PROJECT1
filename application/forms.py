03587fd9c742b3ec8711196c97e0ce8a5461d2c0
majidub2 <majid.mohamed9802@gmail.com>
2022-03-08 23:46:22 +0000

    app is now CRUD functional

--- application/forms.py
+++ application/forms.py
@@ -1,22 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
+from wtforms import StringField, IntegerField, SubmitField, SelectField
 from wtforms.validators import Length, NumberRange, InputRequired
-from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from application.models import reviews,restaurants
+from application.models import Reviews, Restaurants
 
 
 
-class Addreviews(FlaskForm): #many
-    title = StringField("review title", validators=[InputRequired())
-    review = StringField("review body", validators=[InputRequired())
-    rating = IntegerField('Rating (1-5)',validators=[InputRequired(),NumberRange(min=1, max=5)])
+class AddReviews(FlaskForm): #man
+    title = StringField("review title", validators=[InputRequired()])
+    review = StringField("review body", validators=[InputRequired()])
+    rating = IntegerField('Rating (1-10)',validators=[InputRequired(),NumberRange(min=1, max=10)])
     restaurants_ID = SelectField('Restaurant to review', choices=[])
     submit = SubmitField('Submit your review!')
 
-# class Addrestaraunts(FlaskForm):
-#     name = StringField("restaurant name", validators=[InputRequired())
-#     cuisine = StringField("restaurant cuisine", validators=[InputRequired())
-#     submit = SubmitField('Enter Restaurant')
+class AddRestaurants(FlaskForm):
+    name = StringField("restaurant name", validators=[InputRequired()])
+    cuisine = StringField("restaurant cuisine", validators=[InputRequired()])
+    submit = SubmitField('Save Restaurant')
+
 
     
 
